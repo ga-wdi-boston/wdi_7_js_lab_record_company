1f37ddef028fda6831561eb3353398cf4a9c65fa
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-21 18:01:14 -0500

    user can add artist now

--- src/artists.js
+++ src/artists.js
@@ -1,3 +1,25 @@
-var RCApp = RCApp || {
-  
-};
+var RCApp = RCApp || {};
+
+RCApp.addArtist = function(event){
+  var artist = document.createElement('li'),
+      artist_delete_button = document.createElement('delete-button'),
+      artist_list = document.getElementById('artist-list'),
+      artist_counter;
+  artist_counter = parseInt(artist_list.getAttribute('data-counter'));
+  artist.setAttribute('id', 'artist_' + artist_counter);
+
+  artist_delete_button.setAttribute('id', 'artist_delete_button_' + artist_counter);
+
+  artist_counter += 1;
+
+  artist.innerHTML = event.target.value;
+  artist_delete_button.value = 'Delete';
+
+  artist_list.setAttribute('data-counter', artist_counter);
+
+  artist.appendChild(artist_delete_button);
+
+  artist_list.appendChild(artist);
+  event.target.value = '';
+
+}
