c61d07c55163c29abaeccd9c733172859f7ca7ff
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-21 18:22:26 -0500

    user can delete an artist now

--- src/artists.js
+++ src/artists.js
@@ -1,25 +1,54 @@
 var RCApp = RCApp || {};
 
 RCApp.addArtist = function(event){
+
   var artist = document.createElement('li'),
-      artist_delete_button = document.createElement('delete-button'),
+      artist_delete_button = document.createElement('button'),
+      artist_show_button = document.createElement('button'),
+      artist_info = document.createElement('li'),
       artist_list = document.getElementById('artist-list'),
       artist_counter;
+
   artist_counter = parseInt(artist_list.getAttribute('data-counter'));
   artist.setAttribute('id', 'artist_' + artist_counter);
-
+  artist_show_button.setAttribute('id', 'artist_show_button' + artist_counter);
   artist_delete_button.setAttribute('id', 'artist_delete_button_' + artist_counter);
+  artist_info.setAttribute('id', + artist_counter);
 
   artist_counter += 1;
 
-  artist.innerHTML = event.target.value;
-  artist_delete_button.value = 'Delete';
+  artist.innerText = event.target.value + '     ';
+  artist_show_button.innerText = "Show";
+  artist_delete_button.innerText = 'Delete';
 
   artist_list.setAttribute('data-counter', artist_counter);
 
+  artist.appendChild(artist_show_button);
   artist.appendChild(artist_delete_button);
 
   artist_list.appendChild(artist);
   event.target.value = '';
 
 }
+
+RCApp.artistDelete = function(event){
+  var artist_array = event.target.id.split('_'),
+  artist_list = document.getElementById('artist-list'), id, artist;
+
+  id = artist_array[artist_array.length - 1];
+  artist = document.getElementById('artist_' + id);
+  artist_list.removeChild(artist);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
