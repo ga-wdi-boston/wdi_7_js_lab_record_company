ce1aecc7eebab0697fc3527c49127b06c73e9070
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-22 14:03:15 -0500

    can now add description of artists

--- src/Artist.js
+++ src/Artist.js
@@ -1,7 +1,8 @@
 var ArtistList = ArtistList || {};
-artist_array = []
-Artist = function(name){
+artist_arr = []
+Artist = function(name, description){
 	this.name = name;
+	this.description = description;
 };
 
 ArtistList.addArtist = function(event){
@@ -10,23 +11,27 @@ ArtistList.addArtist = function(event){
  var new_one_artist,
  artistElement = document.createElement('li'),
  inputTextName = document.getElementById('input-text-name'),
+ inputTextDescription = document.getElementById('input-text-description'),
  artistElement_button = document.createElement('button'),
  list = document.getElementById('artists-list'),
  artistElement_counter;
- // get the number of items in list from the list
- // data-counter attribute
+
  artistElement_counter = parseInt(list.getAttribute('data-counter'));
  artistElement.setAttribute('id', 'artistElement_'+ artistElement_counter);
- // set the artistElement delete button id
+
  artistElement_button.setAttribute('id', 'artistElement_button_'+ artistElement_counter);
  artistElement_counter += 1;
+
  artistElement.innerText = inputTextName.value;
  artistElement_button.innerText = 'Delete';
- new_one_artist = new Artist(artistElement.innerText)
- artist_array.push(new_one_artist)
+
+ new_one_artist = new Artist(inputTextName.value,inputTextDescription.value)
+ artist_arr.push(new_one_artist)
+
  list.setAttribute('data-counter', artistElement_counter);
  artistElement.appendChild(artistElement_button);
  list.appendChild(artistElement);
+
  inputTextName.value = '';
 };
 
@@ -37,7 +42,7 @@ ArtistList.artistDelete = function(event){
 
   id = artist_array[artist_array.length - 1];
   //artist = event.target.parentNode (same as but it is more fragile)
-  artist = document.getElementById('artist_' + id);
+  artist = document.getElementById('artistElement_' + id);
   list.removeChild(artist);
 };
 
