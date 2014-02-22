a8dc966249478f41da25daed533f8bc35ca090ed
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-22 14:40:03 -0500

    add show function for artist

--- src/artists.js
+++ src/artists.js
@@ -11,13 +11,16 @@ RCApp.addArtist = function(event){
 
   artist_counter = parseInt(artist_list.getAttribute('data-counter'));
   artist.setAttribute('id', 'artist_' + artist_counter);
-  artist_show_button.setAttribute('id', 'artist_show_button' + artist_counter);
+  artist_show_button.setAttribute('id', 'artist_show_button_' + artist_counter);
   artist_delete_button.setAttribute('id', 'artist_delete_button_' + artist_counter);
-  artist_info.setAttribute('id', + artist_counter);
+  artist_info.setAttribute('id', 'artist_info_'+ artist_counter);
 
   artist_counter += 1;
 
-  artist.innerText = event.target.value + '     ';
+
+
+  artist.innerText = artistName.value + '  ';
+  artist.setAttribute('data-info', event.target.value);
   artist_show_button.innerText = "Show";
   artist_delete_button.innerText = 'Delete';
 
@@ -26,8 +29,11 @@ RCApp.addArtist = function(event){
   artist.appendChild(artist_show_button);
   artist.appendChild(artist_delete_button);
 
+
+
   artist_list.appendChild(artist);
   event.target.value = '';
+  artistName.value = '';
 
 }
 
@@ -37,8 +43,26 @@ RCApp.artistDelete = function(event){
 
   id = artist_array[artist_array.length - 1];
   artist = document.getElementById('artist_' + id);
-  artist_list.removeChild(artist);
+  if("artist_delete_button_" + event.target.id.charAt(event.target.id.length -1) === event.target.id){
+    artist_list.removeChild(artist);
+  };
 }
+
+
+RCApp.artistShow = function(event){
+  var artist_array = event.target.id.split('_'),
+  artist_list = document.getElementById('artist-list'), id, artist;
+
+  id = artist_array[artist_array.length - 1];
+  artist = document.getElementById('artist_' + id);
+  artist_info = artist.getAttribute('data-info');
+  artist.innerHTML += ('<br>' + artist_info);
+  if("artist_show_button_" + event.target.id.charAt(event.target.id.length -1) === event.target.id){
+    artist_list.appendChild(artist);
+
+  };
+}
+
 
 
 
