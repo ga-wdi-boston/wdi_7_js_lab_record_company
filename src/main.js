f2a7f8acf94dad2f7bb1d1acf0dd57875f099862
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-22 17:06:18 -0500

    hooked up the delete button for albums

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 window.onload = function() {
   var addArtistButton = document.getElementById('add-artist'),
   		 addAlbumButton = document.getElementById('add-album'),
-                 artists_list = document.getElementById('artists-list');
+         artists_list = document.getElementById('artists-list'),
+          albums_list = document.getElementById('albums-list');
    artists_list.addEventListener('click', ArtistList.artistDelete, false);
    addArtistButton.addEventListener('click', ArtistList.addArtist, false);
    addAlbumButton.addEventListener('click', AlbumList.addAlbum, false);
+   albums_list.addEventListener('click', AlbumList.albumDelete, false);
 };
 
 
